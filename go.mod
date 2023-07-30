2eeb4c065416b08d7b922e38dd54f9736afdb64d
alex <agpelkey@yahoo.com>
2023-07-30 19:06:56 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/agpelkey/uni
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"net/url"
+	"os/exec"
+	"strings"
+	"sync"
+)
+
+type config struct {
+    port int
+    env string
+}
+
+type Backend struct {
+    Addr url.URL
+    IsAlive bool
+    mux sync.RWMutex
+}
+
+type LoadBalancer struct {
+    servers []*Backend
+}
+
+func NewLoadBalancer(servers []*Backend) *LoadBalancer {
+    return &LoadBalancer{
+        servers: servers,
+    }
+}
+
+
+func main() {
+
+}
+
+// fuction to check if each back is up
+func (b Backend) CheckHealth() bool {
+    out, _ := exec.Command("ping", b.Addr.Host).Output()
+    if strings.Contains(string(out), "Destination Host unreachable") {
+        fmt.Println("server down")
+        return false
+    } else {
+        fmt.Println("serve alive")
+    }
+    return true
+}
+
+
+
+
+
